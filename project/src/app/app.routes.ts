72308399b649641cc17fdcb0c4805f22defdfa5c
AlabamaWebp <dimyrp@yandex.ru>
2026-04-26 00:57:23 +0300

    1

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/story-intro/story-intro').then((m) => m.StoryIntro),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
---
+++ project/src/app/pages/story-intro/story-intro.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+type StorySlide = {
+  title: string;
+  region: string;
+  description: string;
+  action: string;
+  background: string;
+  character: string;
+};
+
+@Component({
+  selector: 'app-story-intro',
+  templateUrl: './story-intro.html',
+  styleUrl: './story-intro.scss',
+})
+export class StoryIntro {
+  protected readonly slides: StorySlide[] = [
+    {
+      title: 'Сереброзубая Пампалче',
+      region: 'Республика Марий Эл',
+      description:
+        'Сказка повествует о смелой девочке по имени Сереброзубая Пампалче. Её жизнь захватывающая и интересная. Сегодня вы с ней познакомитесь через образовательную игру.',
+      action: 'Начать игру',
+      background: '/assets/story/fon1.jpg',
+      character: '/assets/story/tolka.png',
+    },
+  ];
+}
