fa85b0ac67fc3c2d54486b51cd917532bb0aa626
AlabamaWebp <dimyrp@yandex.ru>
2026-04-27 20:18:08 +0300

    промежуточный

--- project/src/app/pages/module-4/module-4.ts
+++ project/src/app/pages/module-4/module-4.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-module-4',
+  imports: [RouterLink],
   templateUrl: './module-4.html',
   styleUrl: './module-4.scss',
 })
