d7f87b8f2a525f52ad9098f08a227b845b701613
Goilder <Golder_03@mail.ru>
2026-04-28 23:50:47 +0300

    Добавил работающий плеер для модуля 1 и 3

--- project/src/app/pages/module-1/module-1.ts
+++ project/src/app/pages/module-1/module-1.ts
@@ -7,4 +7,37 @@ import { RouterLink } from '@angular/router';
   templateUrl: './module-1.html',
   styleUrl: './module-1.scss',
 })
-export class Module1Component {}
+export class Module1Component {
+  isVideoPlaying = false;
+
+  openAndPlayVideo(
+    toggle: HTMLInputElement,
+    video: HTMLVideoElement,
+    event: MouseEvent,
+  ): void {
+    event.preventDefault();
+    event.stopPropagation();
+    toggle.checked = true;
+    this.playVideo(video);
+  }
+
+  private playVideo(video: HTMLVideoElement): void {
+    void video.play().catch(() => undefined);
+  }
+
+  pauseVideo(video: HTMLVideoElement): void {
+    video.pause();
+    this.isVideoPlaying = false;
+  }
+
+  toggleVideoPlayback(video: HTMLVideoElement, event: MouseEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (video.paused) {
+      void video.play().catch(() => undefined);
+    } else {
+      video.pause();
+    }
+  }
+}
--- project/src/app/pages/module-3/module-3.ts
+++ project/src/app/pages/module-3/module-3.ts
@@ -7,4 +7,37 @@ import { RouterLink } from '@angular/router';
   templateUrl: './module-3.html',
   styleUrl: './module-3.scss',
 })
-export class Module3Component {}
+export class Module3Component {
+  isVideoPlaying = false;
+
+  openAndPlayVideo(
+    toggle: HTMLInputElement,
+    video: HTMLVideoElement,
+    event: MouseEvent,
+  ): void {
+    event.preventDefault();
+    event.stopPropagation();
+    toggle.checked = true;
+    this.playVideo(video);
+  }
+
+  private playVideo(video: HTMLVideoElement): void {
+    void video.play().catch(() => undefined);
+  }
+
+  pauseVideo(video: HTMLVideoElement): void {
+    video.pause();
+    this.isVideoPlaying = false;
+  }
+
+  toggleVideoPlayback(video: HTMLVideoElement, event: MouseEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (video.paused) {
+      void video.play().catch(() => undefined);
+    } else {
+      video.pause();
+    }
+  }
+}
